0f82f0632fe6766a61eb7d2f67ff69593e7b461e
damian-nestea <damian@hotmail.cz>
2024-08-26 21:36:36 -0300

    feat criação de reducers para login e cadastro na plataforma

--- src/redux/user/reducer.ts
+++ src/redux/user/reducer.ts
@@ -1,9 +1,36 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState: any = [{}];
-
+const initialState: any = {
+  list: [],
+  userOnline: {
+    id: 0,
+    name: "",
+    email: "",
+  },
+  isLogged: false,
+};
 const userSlice = createSlice({
   name: "users",
   initialState,
-  reducers: {},
+  reducers: {
+    register: (state, action: PayloadAction<any>) => {
+      const { name, email, password } = action.payload;
+      state.list.push({ name, email, password });
+      console.log(state.list);
+    },
+    login: (state, action: PayloadAction<any>) => {
+      const { email, password } = action.payload;
+      const user = state.list.find(
+        (user: any) => user.email === email && user.password === password,
+      );
+      if (user) {
+        state.userOnline = user;
+        state.isLogged = true;
+      }
+      console.log(state.userOnline);
+    },
+  },
 });
+
+export const { register, login } = userSlice.actions;
+export default userSlice.reducer;
